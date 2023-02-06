b19d50210de20474ead9d08995e7a08b57d9387a
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-02-06 15:20:05 +0100

    Installed unsplash and initial setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-const App = () => {
-  return <h1>Rendering application</h1>;
-};
+import { GridSection } from "./components/GridSection/GridSection.jsx";
+
+function App() {
+  return (
+    <StrictMode>
+      <GridSection />
+    </StrictMode>
+  );
+}
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(React.createElement(App));
---
+++ src/components/GridSection/GridSection.jsx
@@ -0,0 +1,3 @@
+export function GridSection() {
+  return <h1> I am a nice grid section, how is great, really, really great</h1>;
+}
