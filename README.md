9224e8a3ca3ca7fddb8afde289bdf275dc623181
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-02-06 15:04:47 +0100

    Installed unsplash

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Read API unsplash documentation
+2. Design components
+3. Test request
+4. Integrate library
+5. Layout
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-const App = () => <h1>Rendering</h1>;
+const App = () => {
+  return <h1>Rendering application</h1>;
+};
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
